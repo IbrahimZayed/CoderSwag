ea49f1b9356f85a5211cda7bf7c0c0f0451b5464
Ibrahem Ahmed <31312513+IbrahimZayed@users.noreply.github.com>
2018-05-29 23:33:58 +0200

    ....

--- app/src/main/java/com/example/smith/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smith/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.smith.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
+import com.example.smith.coderswag.Adapters.CategoryAdapter
+import com.example.smith.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.smith.coderswag.Model.Category
 import com.example.smith.coderswag.R
 import com.example.smith.coderswag.Services.DataService
@@ -10,18 +13,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
-        
+
+        val layoutManger =  LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManger
+        categoryListView.setHasFixedSize(true)
+
 
     }
 
